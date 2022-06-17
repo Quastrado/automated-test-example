41886204cb9b1fa2c367ca2dbbd75b3c26e4af09
Quastrado <Ataxyswriter@yandex.ru>
2022-06-17 14:59:06 +0300

    v.1

---
+++ tests/test_homepage.py
@@ -0,0 +1,17 @@
+import time
+
+import pytest
+from pom.homepage_nav import HomepageNav
+
+
+@pytest.mark.usefixtures('setup')
+class TestHomepage:
+
+    def test_nav_links(self):
+        homepage_nav = HomepageNav(self.driver)
+        actual_links = homepage_nav.get_nav_links_text()
+        expected_links = homepage_nav.NAV_LINK_TEXT
+        assert expected_links == actual_links, 'Validate Nav Links text'
+        homepage_nav.get_nav_link_by_name('Beauty').click()
+        time.sleep(5)
+
